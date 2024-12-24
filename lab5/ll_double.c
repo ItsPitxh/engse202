f98b5a6457769240c8a2901b3198bffd01f5fc03
ItsPitxh <pitched.10121@gmail.com>
2024-12-24 11:22:51 +0700

    changes

--- lab5/ll_double.c
+++ lab5/ll_double.c
@@ -1,512 +1,238 @@
-/* Program create DOUBLY CIRCULAR LINKED LIST by...
-1. Create Node N Nodes
-2. Insert node before/after data as defined
-3. Delete node before/itself/after data as defined
-4. Show address of Node
-Note.- Not allow to delete the HEAD Node
-*/
-
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define HeadInfo -999 // Define data of Head Node
-
+// Structure for a Node in Doubly Linked List
 struct Node {
     int info;
     struct Node *llink;
     struct Node *rlink;
 };
 
-struct Node *H, *H1, *p, *q; // Declare pointer node
-int i, j, k, n, data;
-char ch;
+// Global pointers
+struct Node *H = NULL; // Head pointer
 
-Node *Allocate() {
-    struct Node *temp;
-    temp = (Node*)malloc(sizeof(Node)); // Allocate node
+// Function prototypes
+struct Node* Allocate();
+void CreateNNode(int n);
+void ShowAllNode();
+void InsertAfter(int data);
+void InsertBefore(int data);
+void DeleteBefore(int data);
+void DeleteSelf(int data);
+void DeleteAfter(int data);
+
+// Allocate memory for a new node
+struct Node* Allocate() {
+    struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    if (!temp) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
     return temp;
 }
 
+// Create N Nodes with random data
 void CreateNNode(int n) {
-    int i, temp;
-    H1 = H; // Start H1 at Head
-    for (i = 1; i <= n; i++) {
-        p = Allocate(); // Allocate New Node
-        temp = 1 + rand() % 99; // Random difference number 1..99
-        p->info = temp;
-        H1->rlink = p; 
-        p->llink = H1; 
-        H1 = p; 
-        H1->rlink = H;
-        H->llink = H1;
-    }
-}
-
-void ShowAllNode() {
-    printf("H = %x\n", H);
-    p = H;
-    i = 1;
-
-    if (p->rlink != H) {
-        p = p->rlink;
-        while (p != H) {
-            printf("%d) : %x\t", i, p);
-            printf("LLINK : %x\t", p->llink);
-            printf("INFO : %d\t", p->info);
-            printf("RLINK : %x\n", p->rlink);
-            p = p->rlink;
-            i++;
-        }
-    }
-}
-
-void InsertAfter(int data1) {
-    int temp;
-    if (H->rlink == H) {
-        printf("Linked List have no node!!..\n");
-    } else {
-        H1 = H->rlink;
-        while (H1->info != HeadInfo) {
-            if (H1->info == data1) {
-                p = Allocate();
-                printf("\nInsert data : ");
-                scanf("%d", &temp);
-                p->info = temp;
-
-                if (H1->rlink == H) {
-                    p->rlink = H;
-                    H->llink = p;
-                } else {
-                    p->rlink = H1->rlink;
-                    H1->rlink->llink = p;
-                }
-
-                p->llink = H1;
-                H1->rlink = p;
-            }
-            H1 = H1->rlink;
-        }
-    }
-}
-
-void InsertBefore(int data1) {
-    int temp;
-    if (H->rlink == H) {
-        printf("Linked List have no node!!..\n");
-    } else {
-        H1 = H->rlink;
-        while (H1->info != HeadInfo) {
-            if (H1->info == data1) {
-                p = Allocate();
-                printf("\nInsert data : ");
-                scanf("%d", &temp);
-                p->info = temp;
-
-                if (H1->llink == H) {
-                    p->llink = H;
-                    H->rlink = p;
-                } else {
-                    H1->llink->rlink = p;
-                    p->llink = H1->llink;
-                }
-
-                H1->llink = p;
-                p->rlink = H1;
-            }
-            H1 = H1->rlink;
-        }
-    }
-}
-
-void DeleteBefore(int data1) {
-    if (H->rlink == H) {
-        printf("Linked List have NO NODE!!..\n");
-    } else {
-        H1 = H->rlink;
-        while (H1->info != HeadInfo) {
-            if (H1->info == data1) {
-                if (H1->llink == H) {
-                    printf("No more node before here, Can't delete it!!!\n");
-                } else {
-                    p = H1->llink;
-                    H1->llink = p->llink;
-                    p->llink->rlink = H1;
-                    free(p);
-                }
-            }
-            H1 = H1->rlink;
-        }
-    }
-}
-
-void DeleteSelf(int data1) {
-    if (H->rlink == H) {
-        printf("Linked List have NO NODE!!..\n");
-    } else {
-        H1 = H->rlink;
-        while (H1->info != HeadInfo) {
-            if (H1->info == data1) {
-                p = H1;
-
-                if (p->llink == H && p->rlink == H) {
-                    H->llink = H;
-                    H->rlink = H;
-                } else {
-                    p->llink->rlink = p->rlink;
-                    p->rlink->llink = p->llink;
-                }
-
-                free(p);
-            }
-            H1 = H1->rlink;
-        }
-    }
-}
-
-void DeleteAfter(int data1) {
-    if (H->rlink == H) {
-        printf("Linked List have NO NODE!!..\n");
-    } else {
-        H1 = H->rlink;
-        while (H1->info != HeadInfo) {
-            if (H1->info == data1) {
-                if (H1->rlink == H) {
-                    printf("No more node from here, Can't delete it!!!\n");
-                } else {
-                    p = H1->rlink;
-                    H1->rlink = p->rlink;
-                    p->rlink->llink = H1;
-                    free(p);
-                }
-            }
-            H1 = H1->rlink;
-        }
-    }
-}
-
-int main() {
-    p = Allocate();
-    p->info = HeadInfo;
-    p->llink = p;
-    p->rlink = p;
-    H = p;
-
-    n = 10;
-    CreateNNode(n);
-
-    printf("PROGRAM DOUBLY CIRCULAR LINKED LIST \n");
-    printf("================================== \n");
-    printf("All Data in Linked List \n");
-    ShowAllNode();
-
-    ch = ' ';
-    while (ch != 'E') {
-        printf("MENU>> [B:InsertBefore A:InsertAfter\n");
-        printf(" O:DeleteBefore X:DeleteItself D:DeleteAfter E:Exit]\n");
-        ch = getch();
-        switch (ch) {
-            case 'B':
-                printf("\nInsert Before data : ");
-                scanf("%d", &data);
-                InsertBefore(data);
-                printf("\nAll Data in Linked List AFTER INSERTED\n");
-                ShowAllNode();
-                break;
-            case 'A':
-                printf("\nInsert After data : ");
-                scanf("%d", &data);
-                InsertAfter(data);
-                printf("\nAll Data in Linked List AFTER INSERTED\n");
-                ShowAllNode();
-                break;
-            case 'O':
-                printf("\nDelete Before data : ");
-                scanf("%d", &data);
-                DeleteBefore(data);
-                printf("\nAll Data in Linked List AFTER DELETED\n");
-                ShowAllNode();
-                break;
-            case 'X':
-                printf("\nDelete ItSelf data : ");
-                scanf("%d", &data);
-                DeleteSelf(data);
-                printf("\nAll Data in Linked List ITSELF DELETED\n");
-                ShowAllNode();
-                break;
-            case 'D':
-                printf("\nDelete After data : ");
-                scanf("%d", &data);
-                DeleteAfter(data);
-                printf("\nAll Data in Linked List AFTER DELETED\n");
-                ShowAllNode();
-                break;
-        }
-    }
-    return 0;
-}#include <stdio.h>
-#include <stdlib.h>
-
-struct Node {
-    int info;
-    struct Node *llink;
-    struct Node *rlink;
-};
-
-struct Node *H = NULL, *H1 = NULL, *p = NULL, *q = NULL; 
-int i, j, k, n, data;
-char ch;
-
-struct Node *Allocate() {
-    struct Node *temp;
-    temp = (struct Node *)malloc(sizeof(struct Node));
-    return temp;
-}
-
-void CreateNNode(int n) {
-    int temp;
-    H = NULL;
-    H1 = NULL;
-    for (i = 1; i <= n; i++) {
+    struct Node *H1 = NULL, *p = NULL;
+    srand(time(NULL)); // Seed for random numbers
+    for (int i = 1; i <= n; i++) {
         p = Allocate();
-        temp = 1 + rand() % 99;
-        p->info = temp;
-
-        if (i == 1) {
-            H = p;
-        } else {
-            H1->rlink = p;
-        }
-
+        p->info = 1 + rand() % 99; // Random number between 1 and 99
         p->llink = H1;
+        p->rlink = NULL;
+        if (H1) {
+            H1->rlink = p;
+        } else {
+            H = p; // Set head pointer
+        }
         H1 = p;
-        H1->rlink = NULL;
     }
 }
 
+// Display all nodes in the list
 void ShowAllNode() {
-    printf("H = %p\n", H);
-    p = H;
-    i = 1;
-
-    while (p != NULL) {
-        printf("%d) : %p\tLLINK : %p\tINFO : %d\tRLINK : %p\n", 
-            i, p, p->llink, p->info, p->rlink);
+    struct Node *p = H;
+    int i = 1;
+    printf("\nLinked List:\n");
+    if (!p) {
+        printf("The list is empty.\n");
+        return;
+    }
+    while (p) {
+        printf("%d) Address: %p, LLINK: %p, INFO: %d, RLINK: %p\n", i, p, p->llink, p->info, p->rlink);
         p = p->rlink;
         i++;
     }
 }
 
-void InsertAfter(int data1) {
-    int temp;
-
-    if (H == NULL) {
-        printf("Linked List has no nodes!\n");
-        return;
-    }
-
-    H1 = H;
-    while (H1 != NULL) {
-        if (H1->info == data1) {
-            p = Allocate();
-            printf("\nInsert data: ");
-            scanf("%d", &temp);
-            p->info = temp;
-
-            if (H1->rlink == NULL) {
-                p->rlink = NULL;
-            } else {
-                p->rlink = H1->rlink;
-                H1->rlink->llink = p;
+// Insert a node after a specific node
+void InsertAfter(int data) {
+    struct Node *p = H;
+    while (p) {
+        if (p->info == data) {
+            struct Node *newNode = Allocate();
+            printf("Enter data to insert after %d: ", data);
+            scanf("%d", &newNode->info);
+            newNode->rlink = p->rlink;
+            newNode->llink = p;
+            if (p->rlink) {
+                p->rlink->llink = newNode;
             }
-
-            p->llink = H1;
-            H1->rlink = p;
+            p->rlink = newNode;
+            return;
         }
-
-        H1 = H1->rlink;
+        p = p->rlink;
     }
+    printf("Data %d not found in the list.\n", data);
 }
 
-void InsertBefore(int data1) {
-    int temp;
-
-    if (H == NULL) {
-        printf("Linked List has no nodes!\n");
-        return;
-    }
-
-    H1 = H;
-    while (H1 != NULL) {
-        if (H1->info == data1) {
-            p = Allocate();
-            printf("\nInsert data: ");
-            scanf("%d", &temp);
-            p->info = temp;
-
-            if (H1->llink == NULL) {
-                p->llink = NULL;
-                H = p;
+// Insert a node before a specific node
+void InsertBefore(int data) {
+    struct Node *p = H;
+    while (p) {
+        if (p->info == data) {
+            struct Node *newNode = Allocate();
+            printf("Enter data to insert before %d: ", data);
+            scanf("%d", &newNode->info);
+            newNode->rlink = p;
+            newNode->llink = p->llink;
+            if (p->llink) {
+                p->llink->rlink = newNode;
             } else {
-                H1->llink->rlink = p;
-                p->llink = H1->llink;
+                H = newNode; // Update head pointer
             }
-
-            H1->llink = p;
-            p->rlink = H1;
+            p->llink = newNode;
+            return;
         }
-
-        H1 = H1->rlink;
+        p = p->rlink;
     }
+    printf("Data %d not found in the list.\n", data);
 }
 
-void DeleteBefore(int data1) {
-    if (H == NULL) {
-        printf("Linked List has no nodes!\n");
-        return;
-    }
-
-    H1 = H;
-    while (H1 != NULL) {
-        if (H1->info == data1) {
-            if (H1->llink == NULL) {
-                printf("No node before this, can't delete!\n");
-            } else {
-                p = H1->llink;
-                if (p->llink == NULL) {
-                    H1->llink = NULL;
-                    H = H1;
-                } else {
-                    H1->llink = p->llink;
-                    p->llink->rlink = H1;
-                }
-
-                free(p);
+// Delete the node before a specific node
+void DeleteBefore(int data) {
+    struct Node *p = H;
+    while (p) {
+        if (p->info == data) {
+            if (!p->llink) {
+                printf("No node exists before %d.\n", data);
+                return;
             }
+            struct Node *temp = p->llink;
+            if (temp->llink) {
+                temp->llink->rlink = p;
+            } else {
+                H = p; // Update head pointer
+            }
+            p->llink = temp->llink;
+            free(temp);
+            return;
         }
-
-        H1 = H1->rlink;
+        p = p->rlink;
     }
+    printf("Data %d not found in the list.\n", data);
 }
 
-void DeleteSelf(int data1) {
-    if (H == NULL) {
-        printf("Linked List has no nodes!\n");
-        return;
-    }
-
-    H1 = H;
-    while (H1 != NULL) {
-        if (H1->info == data1) {
-            p = H1;
-
-            if (p->llink == NULL && p->rlink == NULL) {
-                H = NULL;
-            } else if (p->llink == NULL) {
-                H = p->rlink;
-                H->llink = NULL;
-            } else if (p->rlink == NULL) {
-                p->llink->rlink = NULL;
-            } else {
+// Delete the node itself
+void DeleteSelf(int data) {
+    struct Node *p = H;
+    while (p) {
+        if (p->info == data) {
+            if (p->llink) {
                 p->llink->rlink = p->rlink;
+            } else {
+                H = p->rlink; // Update head pointer
+            }
+            if (p->rlink) {
                 p->rlink->llink = p->llink;
             }
-
             free(p);
+            return;
         }
-
-        H1 = H1->rlink;
+        p = p->rlink;
     }
+    printf("Data %d not found in the list.\n", data);
 }
 
-void DeleteAfter(int data1) {
-    if (H == NULL) {
-        printf("Linked List has no nodes!\n");
-        return;
-    }
-
-    H1 = H;
-    while (H1 != NULL) {
-        if (H1->info == data1) {
-            if (H1->rlink == NULL) {
-                printf("No node after this, can't delete!\n");
-            } else {
-                p = H1->rlink;
-
-                if (p->rlink == NULL) {
-                    H1->rlink = NULL;
-                } else {
-                    H1->rlink = p->rlink;
-                    p->rlink->llink = H1;
-                }
-
-                free(p);
+// Delete the node after a specific node
+void DeleteAfter(int data) {
+    struct Node *p = H;
+    while (p) {
+        if (p->info == data) {
+            if (!p->rlink) {
+                printf("No node exists after %d.\n", data);
+                return;
             }
+            struct Node *temp = p->rlink;
+            p->rlink = temp->rlink;
+            if (temp->rlink) {
+                temp->rlink->llink = p;
+            }
+            free(temp);
+            return;
         }
-
-        H1 = H1->rlink;
+        p = p->rlink;
     }
+    printf("Data %d not found in the list.\n", data);
 }
 
+// Main function
 int main() {
-    n = 10;
-    CreateNNode(n);
+    int n = 10, data;
+    char choice;
 
-    printf("PROGRAM DOUBLY LINKED LIST\n");
-    printf("==========================\n");
-    printf("All Data in Linked List\n");
+    printf("Creating a doubly linked list with %d nodes.\n", n);
+    CreateNNode(n);
     ShowAllNode();
 
     while (1) {
-        printf("MENU >> [B: InsertBefore A: InsertAfter\n");
-        printf("O: DeleteBefore X: DeleteSelf\n");
-        printf("D: DeleteAfter E: Exit]\n");
+        printf("\nMENU: [B: Insert Before | A: Insert After | O: Delete Before | X: Delete Itself | D: Delete After | E: Exit]\n");
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
 
-        ch = getchar();
-        if (ch == 'E') break;
-
-        switch (ch) {
+        switch (choice) {
             case 'B':
-                printf("\nInsert Before data: ");
+                printf("Enter the data to insert before: ");
                 scanf("%d", &data);
                 InsertBefore(data);
-                printf("\nAll Data in Linked List AFTER INSERTION\n");
                 ShowAllNode();
                 break;
 
             case 'A':
-                printf("\nInsert After data: ");
+                printf("Enter the data to insert after: ");
                 scanf("%d", &data);
                 InsertAfter(data);
-                printf("\nAll Data in Linked List AFTER INSERTION\n");
                 ShowAllNode();
                 break;
 
             case 'O':
-                printf("\nDelete Before data: ");
+                printf("Enter the data to delete before: ");
                 scanf("%d", &data);
                 DeleteBefore(data);
-                printf("\nAll Data in Linked List AFTER DELETION\n");
                 ShowAllNode();
                 break;
 
             case 'X':
-                printf("\nDelete Self data: ");
+                printf("Enter the data to delete itself: ");
                 scanf("%d", &data);
                 DeleteSelf(data);
-                printf("\nAll Data in Linked List AFTER SELF-DELETION\n");
                 ShowAllNode();
                 break;
 
             case 'D':
-                printf("\nDelete After data: ");
+                printf("Enter the data to delete after: ");
                 scanf("%d", &data);
                 DeleteAfter(data);
-                printf("\nAll Data in Linked List AFTER DELETION\n");
                 ShowAllNode();
                 break;
+
+            case 'E':
+                printf("Exiting program.\n");
+                return 0;
+
+            default:
+                printf("Invalid choice. Try again.\n");
         }
     }
-
-    return 0;
 }
